c72d3bccc8a4390e9ec492db4d7acd724ba6d420
@Yazanwidyan <yazanwidyan1@gmail.com>
2023-11-26 15:05:21 +0300

    fix

--- src/components/pages/user/home-user/HomeUser.tsx
+++ src/components/pages/user/home-user/HomeUser.tsx
@@ -1,17 +1,19 @@
 import { useContext, useEffect, useState } from "react";
+import { Button } from "@chakra-ui/button";
+import { Grid, GridItem, SkeletonText } from "@chakra-ui/react";
 import ServiceProviderCard from "../../../organisms/service-provider-card/ServiceProviderCard";
 import CreateEvent from "../create-event/CreateEvent";
-import { Button } from "@chakra-ui/button";
-import { Grid, GridItem } from "@chakra-ui/react";
 import { UserContext } from "../../../../contexts/UserContext";
 import userServices from "../../../../services/userServices";
 import useCustomToast from "../../../../hooks/useCustomToast";
+import { Skeleton } from "@chakra-ui/react";
 
 const HomeUser = () => {
   const { user } = useContext(UserContext);
   const { showToast } = useCustomToast();
 
   const [providersList, setListProviders] = useState<any>([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const openDialog = () => {
@@ -24,7 +26,6 @@ const HomeUser = () => {
 
   useEffect(() => {
     fetchData();
-    return () => {};
   }, []);
 
   const fetchData = async () => {
@@ -36,26 +37,47 @@ const HomeUser = () => {
       setListProviders(res.data);
     } catch (error) {
       showToast(error, { status: "error" });
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <div>
-      <Grid templateColumns="repeat(auto-fill, minmax(300px, 1fr))" gap={4}>
-        {providersList.map((userProfile, index) => (
-          <GridItem key={index}>
-            <ServiceProviderCard
-              image={userProfile.profilePic}
-              title={userProfile.title}
-              name={userProfile.username}
-              description={userProfile.bio}
-              experience={userProfile.experience}
-              nationality={userProfile.nationality}
-              gender={userProfile.gender}
-              providerID={userProfile.id}
-            />
-          </GridItem>
-        ))}
+      <Grid templateColumns="repeat(auto-fill, minmax(400px, 1fr))" gap={4}>
+        {isLoading
+          ? Array.from({ length: 5 }).map((_, index) => (
+              <GridItem key={index}>
+                <Skeleton height="300px" />
+                <SkeletonText
+                  mt="4"
+                  noOfLines={4}
+                  spacing="4"
+                  skeletonHeight="2"
+                />
+                <SkeletonText
+                  mt="4"
+                  noOfLines={4}
+                  spacing="4"
+                  skeletonHeight="2"
+                />
+                <Skeleton mt={4} height="40px" width="200px" />
+              </GridItem>
+            ))
+          : providersList.map((userProfile, index) => (
+              <GridItem key={index}>
+                <ServiceProviderCard
+                  image={userProfile.profilePic}
+                  title={userProfile.title}
+                  name={userProfile.username}
+                  description={userProfile.bio}
+                  experience={userProfile.experience}
+                  nationality={userProfile.nationality}
+                  gender={userProfile.gender}
+                  providerID={userProfile.id}
+                />
+              </GridItem>
+            ))}
       </Grid>
       <Button onClick={openDialog}>Create Event</Button>
 
